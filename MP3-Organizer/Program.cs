0558a0fd1709cfe8b385861170128cf1172de75f
ArmaPsyko <armapsyko69420@outlook.com>
2024-06-02 11:51:05 +0200

    initial release

---
+++ MP3-Organizer/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace MP3_Organizer
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // Define some locals
+            List<string> allowed = new List<string>();
+
+            allowed.Add(".flac");
+            allowed.Add(".mp3");
+            allowed.Add(".m4a");
+
+            string startIn = "", destination = "";
+            bool errors = false;
+
+            // Print welcome message
+            Console.WriteLine("   ___      _ _        __ _       " + Environment.NewLine +
+                "  |_  |    | | |      / _(_)      " + Environment.NewLine +
+                "    | | ___| | |_   _| |_ _ _ __  " + Environment.NewLine +
+                "    | |/ _ \\ | | | | |  _| | '_ \\ " + Environment.NewLine +
+                "/\\__/ /  __/ | | |_| | | | | | | |" + Environment.NewLine +
+                "\\____/ \\___|_|_|\\__, |_| |_|_| |_|" + Environment.NewLine +
+                "                 __/ |            " + Environment.NewLine +
+                "                |___/         " + Environment.NewLine +
+                " " + Environment.NewLine +
+                              "Welcome to the Jellyfin song indexing tool. This tool indexes your music " + Environment.NewLine +
+                              "specified by fileformat. Everything is based on your IDv3 tags. " + Environment.NewLine +
+                              "If your tags don't match, the tool will mess up the copying. Be " + Environment.NewLine +
+                              "very careful with your indexing scope!");
+            Console.WriteLine(Environment.NewLine);
+
+            // Print allowed formats
+            Console.WriteLine("Allowed file formats supported are:");
+
+            foreach (string format in allowed)
+            {
+                Console.WriteLine(string.Format(@" - {0}", format));
+            }
+
+            Console.Write(Environment.NewLine);
+
+            // Display target location folder dialog
+            Console.WriteLine("Define a target location for the renamed track...");
+            FolderBrowserDialog targetLocationDialog = new FolderBrowserDialog();
+            targetLocationDialog.Description = "Select the folder to copy the renamed tracks to";
+            targetLocationDialog.ShowNewFolderButton = true;
+            targetLocationDialog.RootFolder = Environment.SpecialFolder.MyComputer;
+
+            if (targetLocationDialog.ShowDialog() != DialogResult.OK)
+            {
+                Console.WriteLine("User abort. Exiting...");
+                Environment.Exit(0);
+            }
+
+            destination = targetLocationDialog.SelectedPath;
+
+            Console.WriteLine(string.Format(@"Chosen target path is {0}", destination));
+            Console.Write(Environment.NewLine);
+
+            // Display start location folder dialog
+            Console.WriteLine("Input start directory...");
+            FolderBrowserDialog startInDialog = new FolderBrowserDialog();
+            startInDialog.Description = "Select the folder to extract from";
+            startInDialog.ShowNewFolderButton = false;
+            startInDialog.RootFolder = Environment.SpecialFolder.MyComputer;
+
+            if (startInDialog.ShowDialog() != DialogResult.OK)
+            {
+                Console.WriteLine("User abort. Exiting...");
+                Environment.Exit(0);
+            }
+
+            startIn = startInDialog.SelectedPath;
+
+            Console.WriteLine(string.Format(@"Chosen start path is {0}", startIn));
+            Console.Write(Environment.NewLine);
+
+            // Start organizing crap
+            TrackOrganizer organizer = new TrackOrganizer(startIn, destination, allowed);
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            Console.WriteLine("Start indexing and organizing procedure...");
+            Console.Write(Environment.NewLine);
+            try
+            {
+                organizer.Index();
+                organizer.Organize();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                errors = true;
+            }
+
+            Console.Write(Environment.NewLine);
+
+            stopwatch.Stop();
+
+            // Print some nice stats
+            if (!errors)
+            {
+                Console.WriteLine(string.Format(@"{0} tracks were renamed and moved to the target location.", organizer.Files.Count));
+                Console.WriteLine(string.Format(@"All tracks were indexed and organized in {0}.", stopwatch.Elapsed));
+                Console.Write(Environment.NewLine);
+            }
+
+            // And wait for last user input
+            Console.WriteLine("Done! Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MP3-Organizer/TrackOrganizer.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Globalization;
+using TagLib;
+
+namespace MP3_Organizer
+{
+    class TrackOrganizer
+    {
+        private List<string> _files = new List<string>();
+        private List<string> _allowed = new List<string>();
+        private TextInfo _textInfo;
+        private string _startIn;
+        private string _destination;
+
+        #region Accessors
+
+        public List<string> Files
+        {
+            get { return _files; }
+            set { _files = value; }
+        }
+
+        public List<string> Allowed
+        {
+            get { return _allowed; }
+            set { _allowed = value; }
+        }
+
+        public TextInfo TextInfo
+        {
+            get { return _textInfo; }
+            set { _textInfo = value; }
+        }
+
+        public string StartIn
+        {
+            get { return _startIn; }
+            set { _startIn = value; }
+        }
+
+        public string Destination
+        {
+            get { return _destination; }
+            set { _destination = value; }
+        }
+
+        #endregion
+
+        public TrackOrganizer(string startIn, string destination, List<string> allowed)
+        {
+            StartIn = startIn;
+            Destination = destination;
+            Allowed = allowed;
+
+            TextInfo = new CultureInfo("nl-NL", false).TextInfo;
+        }
+
+        public void Index()
+        {
+            DirectorySearch(StartIn);
+        }
+
+        public void Organize()
+        {
+            if (Files.Count == 0)
+            {
+                Console.WriteLine("Run Index() first!");
+                return;
+            }
+
+            foreach (string file in Files)
+            {
+                // Update the metadata
+                UpdateMetadata(file);
+
+                // Create file path and name
+                string destFilePath = CreateDirectoryPath(file);
+                string destFileName = CreateFilePath(file);
+
+                // Check if directory exists, otherwise create it
+                if (!System.IO.Directory.Exists(Destination + destFilePath))
+                {
+                    System.IO.Directory.CreateDirectory(Destination + destFilePath);
+                }
+
+                // Create full file path
+                string fullPath = Destination + destFilePath + destFileName;
+
+                Console.WriteLine(fullPath);
+
+                // Check if file exists, otherwise copy file to new location
+                if (!System.IO.File.Exists(fullPath))
+                {
+                    System.IO.File.Copy(file, fullPath);
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            foreach (string file in Files)
+            {
+                builder.AppendLine(file);
+            }
+
+            return builder.ToString();
+        }
+
+        private void UpdateMetadata(string trackPath)
+        {
+            TagLib.File track = TagLib.File.Create(trackPath);
+
+            bool hasChanges = false;
+
+            // Update Performers
+            if (track.Tag.Performers.Length > 0)
+            {
+                string performers = string.Join(",", track.Tag.Performers);
+                if (performers.Contains(","))
+                {
+                    track.Tag.Performers = performers.Replace(",", ";").Split(';');
+                    hasChanges = true;
+                }
+            }
+
+            // Update Album
+            if (!string.IsNullOrEmpty(track.Tag.Album))
+            {
+                string album = track.Tag.Album;
+                if (album.Contains(","))
+                {
+                    track.Tag.Album = album.Replace(",", ";");
+                    hasChanges = true;
+                }
+            }
+
+            if (hasChanges)
+            {
+                track.Save();
+                Console.WriteLine($"Updated metadata: {trackPath}");
+            }
+
+            track.Dispose();
+        }
+
+        private string CreateDirectoryPath(string trackPath)
+        {
+            TagLib.File track = TagLib.File.Create(trackPath);
+
+            if (track.Tag == null)
+            {
+                throw new Exception(string.Format("This track seems to be invalid and can't be read ({0})", trackPath));
+            }
+
+            if (track.Tag.Performers == null)
+            {
+                throw new Exception(string.Format("This track contains no artist information ({0})", trackPath));
+            }
+
+            if (track.Tag.Album == null)
+            {
+                throw new Exception(string.Format("This track contains no album information ({0})", trackPath));
+            }
+
+            string invalid = new string(System.IO.Path.GetInvalidPathChars());
+            string performer = track.Tag.Performers[0]; // Use only the first artist
+
+            string album = track.Tag.Album;
+
+            track.Dispose();
+
+            foreach (char c in invalid)
+            {
+                performer = performer.Replace(c.ToString(), "");
+                album = album.Replace(c.ToString(), "");
+            }
+
+            performer = TextInfo.ToTitleCase(performer.ToLower()).Replace("/", "-").Replace("?", "").Replace(":", "");
+            album = TextInfo.ToTitleCase(album.ToLower()).Replace("/", "-").Replace("?", "").Replace(":", "");
+
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendFormat(@"\{0}\{1}\", performer, album);
+
+            return builder.ToString();
+        }
+
+        private string CreateFilePath(string trackPath)
+        {
+            TagLib.File track = TagLib.File.Create(trackPath);
+
+            string extension = System.IO.Path.GetExtension(trackPath);
+            string invalid = new string(System.IO.Path.GetInvalidFileNameChars());
+
+            if (track.Tag.Title == null)
+            {
+                throw new Exception(string.Format("This track contains no title information ({0})", trackPath));
+            }
+
+            string title = track.Tag.Title;
+            string number = track.Tag.Track.ToString("D2");
+
+            track.Dispose();
+
+            foreach (char c in invalid)
+            {
+                title = title.Replace(c.ToString(), "");
+            }
+
+            title = TextInfo.ToTitleCase(title.ToLower());
+
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendFormat(@"{2} - {0}{1}", title, extension, number);
+
+            return builder.ToString();
+        }
+
+        private void DirectorySearch(string startIn)
+        {
+            try
+            {
+                foreach (string file in System.IO.Directory.GetFiles(startIn))
+                {
+                    string extension = System.IO.Path.GetExtension(file);
+
+                    if (Allowed.Contains(extension))
+                    {
+                        Files.Add(file);
+                    }
+                }
+
+                foreach (string directory in System.IO.Directory.GetDirectories(startIn))
+                {
+                    DirectorySearch(directory);
+                }
+            }
+            catch (SystemException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
